c8c402ad8e279cd7e5d3b4c8348fa9993ab4e66e
UrielMM <moyauriel05@gmail.com>
2022-06-09 13:11:14 -0500

    Modified the 'dashboard' page

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -15,29 +15,4 @@ class Dashboard extends MY_Controller {
 
    }
 
-   public function SaveUser(){
-
-     $nombre = $this->input->post('nombre');
-     $apaterno = $this->input->post('apaterno');
-     $amaterno = $this->input->post('amaterno');
-     $telefono = $this->input->post('telefono');
-     $email = $this->input->post('email');
-     $username = $this->input->post('username');
-     $password = $this->input->post('password');
-     $ocupacion = $this->input->post('ocupacion');
-
-     $datosusuario = array(
-        'nombre' => $nombre,
-        'apaterno' => $apaterno,
-        'amaterno' => $amaterno,
-        'telefono' => $telefono,
-        'email' => $email,
-        'username' => $username,
-        'password' => $password,
-        'ocupacion' => $ocupacion
-    );
-
-     $this->Query_Model->InsertUsuario($datosusuario);
-   }
-
 }
